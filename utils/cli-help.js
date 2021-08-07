110a2ad24b99c02bc098e763e79b8aea830fbf29
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2021-08-07 11:54:07 -0700

    improve help command

--- utils/cli-help.js
+++ utils/cli-help.js
@@ -2,16 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function() {
-  // fs.readFile(__dirname + '/package.json', function(err, data) {
-  //   if (err) throw err;
-  //   const package = JSON.parse(data);
-  //   // console.log(`for help, see the GitHub page at : ${package}`);
-  //   console.log(package);
-  // });
-  fs.readFile(path.join(__dirname, '..', 'package.json'), function(err, data) {
+  fs.readFile(path.join(__dirname, '..', 'README.md'), function(err, data) {
     if (err) throw err;
-    const package = JSON.parse(data);
-    console.log(`for help, see the GitHub page at : ${package.version}`);
-    console.log(package);
+    const readme = data.toString();
+    console.log(readme);
   });
 }
