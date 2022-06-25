ab231e619c7ed7e3b29e11a5634e99f653b41591
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-06-25 00:54:16 -0700

    convert promises to async / await

--- utils/Twitter.js
+++ utils/Twitter.js
@@ -1,9 +1,9 @@
 const https = require('https');
-const axios = require('axios');
+const axios = require('axios').default;
 
 class Twitter {
   constructor(config) {
-    this.api = axios.default.create({
+    this.api = axios.create({
       baseURL: 'https://api.twitter.com/2',
       headers: {
         Authorization: `Bearer ${config.bearerToken}`,
@@ -12,88 +12,101 @@ class Twitter {
     });
   }
   
-  queryTweet(tweetId) {
-    return new Promise((resolve, reject) => {
-      this.api.get(`tweets/${tweetId}`, {
+  async queryTweet(tweetId) {
+    try {
+      const res = await this.api.get(`tweets/${tweetId}`, {
         params: { 'tweet.fields': 'public_metrics' }
-      })
-        .then(res => resolve(res.data.data))
-        .catch(err => reject(err))
-    });
+      });
+      const data = res.data.data;
+      return data;
+    } catch (err) {
+      return err;
+    }
   }
   
-  queryUserById(userId) {
-    return new Promise((resolve, reject) => {
-      this.api.get(`users/${userId}`, {
+  async queryUserById(userId) {
+    try {
+      const res = await this.api.get(`users/${userId}`, {
         params: { 'user.fields': 'public_metrics' }
-      })
-        .then(res => {
-          if (res.data.data) resolve(res.data.data);
-          throw `could not find any data for user id : ${userId}`;
-        })
-        .catch(err => reject(err))
-    });
-  }
-  queryByUserName(userName) {
-    return new Promise((resolve, reject) => {
-      this.api.get(`users/by/username/${userName}`, {
-        params: { 'user.fields': 'public_metrics' }
-      })
-        .then(res => {
-          if (res.data.data) resolve(res.data.data);
-          throw `could not find any data for user name : ${userName}`;
-        })
-        .catch(err => reject(err))
-    });
+      });
+      const data = res.data;
+      return data;
+    } catch (err) {
+      console.log(`could not find any data for user id : ${userId}`);
+      return err;
+    }
   }
 
-  getTweets(id, pageToken = null) {
-    return new Promise((resolve, reject) => {
-      if (!id) reject('need to supply user id ... ')
+  async queryByUserName(userName) {
+    try {
+      const res = await this.api.get(`users/by/username/${userName}`, {
+        params: { 'user.fields': 'public_metrics' }
+      });
+      const data = res.data.data;
+      return data;
+    } catch (err) {
+      console.log(`could not find any data for user name : ${userName}`);
+      return err;
+    }
+  }
+
+  async getTweets(userId, pageToken = null) {
+    try {
+      if (!userId) throw new Error('need to supply user id');
       const params = { max_results: 100, 'tweet.fields': 'public_metrics,created_at' };
-      if (pageToken) { params.pagination_token = pageToken };
-      this.api.get(`users/${id}/tweets`, { params: params })
-        .then(res => resolve(res.data))
-        .catch(err => reject(err))
-    });
+      if (pageToken) { params.pagination_token = pageToken; }
+      const res = await this.api.get(`users/${userId}/tweets`, { params: params });
+      const data = res.data;
+      return data;
+    } catch (err) {
+      return err;
+    }
   }
   
-  getFollowers(userId) {
-    return new Promise((resolve, reject) => {
-      this.api.get(`users/${userId}/followers`)
-        .then(res => resolve(res.data.data))
-        .catch(err => reject(err))
-    })
+  async getFollowers(userId) {
+    try {
+      const res = await this.api.get(`users/${userId}/followers`);
+      const data = res.data.data;
+      return data;
+    } catch(err) {
+      return err;
+    }
   }
   
-  setStreamRules(rules) {
+  async setStreamRules(rules) {
     // rules are in the form : [ { value: 'rule syntax', tag: 'rule description' }, ... ]
-    return new Promise((resolve, reject) => {
-      this.api.post('tweets/search/stream/rules', {
+    try {
+      const res = await this.api.post(`tweets/search/stream/rules`, {
         add: rules
-      })
-        .then(res => resolve(res.data))
-        .catch(err => reject(err))
-    });
+      });
+      const data = res;
+      return data;
+    } catch(err) {
+      return err;
+    }
   }
 
-  getStreamRules() {
-    return new Promise((resolve, reject) => {
-      this.api.get('tweets/search/stream/rules')
-        .then(res => resolve(res.data))
-        .catch(err => reject(err))
-    });
+  async getStreamRules() {
+    try {
+      const res = await this.api.get(`tweets/search/stream/rules`);
+      const data = res.data;
+      return data;
+    } catch(err) {
+      return err;
+    }
   }
   
-  deleteStreamRules(rules) {
+  async deleteStreamRules(rules) {
     // rules are in the form [ 0000, 0000, ... ]
-    return new Promise((resolve, reject) => {
-      this.api.post('tweets/search/stream/rules', {
+    try {
+      const res = await this.api.post(`tweets/search/stream/rules`, {
         delete: { ids: rules }
-      })
-        .then(res => resolve(res.data))
-        .catch(err => reject(err))
-    });
+      });
+      const data = res.data;
+      return data;
+    } catch (err) {
+      return err;
+    }
   }
 
   async stream(filters) {
@@ -108,12 +121,11 @@ class Twitter {
         host: 'api.twitter.com',
         path: params.length > 1 ? '/2/tweets/search/stream?' + params : '/2/tweets/search/stream',
         headers: {
-          Authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAACT2KwEAAAAAMQbgehZGgXV%2FTnjXf2MuqKrOvTs%3Da0Ed15ZWrOB1t6DO8Ql0kwrnYYYE5EghMZK24CC2adoxxulqwV'
+          Authorization: 'Bearer ' + process.env.TWITTER_BEARER_TOKEN
         }
       };
 
       https.get(options, function(response) {
-        let data = '';
         response.on('data', function(chunk) {
           try {
             const data = JSON.parse(chunk);
