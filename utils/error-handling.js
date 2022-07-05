b245af450d2b53f94a11d1190983bd20003b9212
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-07-04 23:19:48 -0700

    use colors in error handling

--- utils/error-handling.js
+++ utils/error-handling.js
@@ -1,4 +1,5 @@
 const util = require('util');
+const { Reset, FgRed, Bright, FgYellow } = require('../colors');
 
 module.exports = function(err) {
   if (Object.keys(err).includes('isAxiosError')) {
@@ -6,44 +7,46 @@ module.exports = function(err) {
     const { status, statusText } = err.response;
     const { baseURL, url, method, headers, data } = err.response.config;
 
-    console.log('Received Axios error');
+    console.log(FgRed + Bright + 'Received Axios error' + Reset);
     console.log('');
     console.log('');
 
-    console.log(`status : ${status}`);
-    console.log(`status text : ${statusText}`);
-    console.log(message);
+    console.log(`${FgYellow}${Bright}status : ${Reset}${status}`);
+    console.log(`${FgYellow}${Bright}status text : ${Reset}${statusText}`);
+    console.log(FgRed + message + Reset);
     console.log('');
     console.log('');
 
-    console.log('request : ');
-    console.log(`base url : ${baseURL}`)
-    console.log(`endpoing : ${url}`)
-    console.log(`method : ${method}`);
-    console.log(`header:`);
+    console.log(FgYellow + Bright + 'REQUEST : ' + Reset);
+    console.log(`\t${FgYellow}base url : ${Reset}${baseURL}`)
+    console.log(`\t${FgYellow}endpoing : ${Reset}${url}`)
+    console.log(`\t${FgYellow}method : ${Reset}${method}`);
+    console.log(`\t${FgYellow}header:${Reset}`);
     Object.keys(headers).forEach(key => {
-      console.log(`\t${key} : ${headers[key]}`);
+      console.log(`\t\t${key} : ${headers[key]}`);
     });
-    console.log('body : ');
-    console.log(data);
+    console.log(FgYellow + Bright + '\tbody : ' + Reset);
+    if (data) {
+      console.log('\t' + JSON.stringify(data, null, 4));
+    }
 
     console.log('');
     console.log('');
 
-    console.log('response : ');
+    console.log(FgYellow + Bright + 'RESPONSE : ' + Reset);
     console.log(util.inspect(err.response.data, { depth: null, colors: true }));
 
     console.log('');
     console.log('');
 
   } else if (Object.keys(err).includes('errors')) {
-    console.log('Twitter error');
+    console.log(FgRed + Bright + 'Twitter error' + Reset);
     console.log('');
     err.errors.forEach(e => {
       const { title, type, detail, value } = e;
-      console.log(`title:\t${title}`);
-      console.log(`type:\t${type}`);
-      console.log(`detail:\t${detail}`);
+      console.log(`${FgYellow}${Bright}title:${Reset}\t${title}`);
+      console.log(`${FgYellow}${Bright}type:${Reset}\t${type}`);
+      console.log(`${FgYellow}${Bright}detail:${Reset}\t${detail}`);
       console.log('');
       console.log('');
     });
