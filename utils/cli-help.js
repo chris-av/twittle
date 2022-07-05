52891d9f144e314ec7fb70e3aeb1c09ab52721e0
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-07-05 01:03:54 -0700

    output help.txt instead of readme

--- utils/cli-help.js
+++ utils/cli-help.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function() {
-  fs.readFile(path.join(__dirname, '..', 'README.md'), function(err, data) {
+  fs.readFile(path.join(__dirname, '..', 'help.txt'), function(err, data) {
     if (err) throw err;
     const readme = data.toString();
     console.log(readme);
